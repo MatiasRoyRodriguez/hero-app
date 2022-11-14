5e12db1665877348a3e33bc52502ba673d0835b8
Matías Roy Rodriguez <matiasrr980@gmail.com>
2022-11-14 12:41:28 -0300

    feat: change card styles

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import './HeroCard.css';
 
 const CharactersByHero = ({ alter_ego, characters }) => {
 
@@ -18,27 +19,24 @@ export const HeroCard = ({
 	const heroImageUrl = `/assets/${id}.jpg`;
 
 	return (
-		<div className="col animate__animated animate__fadeIn">
-			<div className="card">
-				<div className="row no-gutters">
-					<div className="col-4 " >
-						<img src={heroImageUrl} alt={superhero} className="card-img" />
-					</div>
-					<div className="col-8">
-						<div className="card-body">
-							<h5 className="card-title">{superhero}</h5>
-							<p className="card-text">{alter_ego}</p>
+
+		<Link to={`/hero/${id}`} className="my-card">
+			<img src={`./assets/${id}.jpg`} className="img img-responsive" alt={superhero} />
+			<div className="profile-name">{superhero}</div>
+			<div className="profile-position">{alter_ego}</div>
+			<div className="profile-overview">
+				<div className="profile-overview">
+					<div className="row">
+						<div className="col-ms-4">
+							<h3>{publisher}</h3>
 							<CharactersByHero alter_ego={alter_ego} characters={characters} />
 							<p className="card-text">
 								<small className="text-muted">{first_appearance}</small>
 							</p>
-							<Link to={`/hero/${id}`}>
-								Más..
-							</Link>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</Link>
 	)
 }
